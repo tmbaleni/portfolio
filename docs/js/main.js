ba99463bd44a3aa0a5ace01cf2ce3682d142c08f
Tabo Mbaleni <tmbaleni@live.com>
2019-10-16 12:52:31 +0800

    Fixed some styles

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
   $(".owl-carousel").owlCarousel({
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+
     loop:true,
-    margin:9,
+    margin:10,
     responsiveClass:true,
     responsive:{
         0:{
-            items:1,
-            nav:true
+            items:1
         },
         600:{
-            items:3,
-            nav:false
+            items:2
         },
         1000:{
-            items:4,
-            nav:true,
-            loop:false
+            items:3
         }
     }
   });
